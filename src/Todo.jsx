a5382fc4d81147afc682359520b85b0e867fc8e4
choiyui <choiyj220220@gmail.com>
2026-07-01 13:34:07 +0900

    feat: implement checkbox functionality in Todo component for item completion

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export default function Todo({ data }) {
+  const [isChecked, setIsChecked] = useState(data.checked);
+  const handleChecked = () => {
+    setIsChecked(prev => !prev);
+  };
   return (
     <div className="d-flex justify-content-between mb-2">
       <Form.Check // prettier-ignore
         type="checkbox"
+        checked={data.checked}
         id={`todo-${data.id}`}
         label={data.title}
+        onChange={handleChecked}
       />
       <div className="d-flex gap-2">
         <Button variant="secondary" size="sm">
