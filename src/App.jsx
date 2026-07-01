ff6f8aef14789936fb6465bf594c4bc88d4df1f0
choiyui <choiyj220220@gmail.com>
2026-07-01 10:37:19 +0900

    feat: update dependencies and enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function App() {
-
   return (
     <>
-    <h1>My todo App</h1>
+      <div className="container">
+        <h1>My todo App</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Button variant="primary">Primary</Button>
+        </Form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
