f9b878408381eec079f34c8bc7365e1a872d2aad
choiyui <choiyj220220@gmail.com>
2026-07-01 13:18:14 +0900

    feat: refactor App component to enhance todo item addition and input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useState } from "react";
+import { useState, useMemo, useRef } from "react";
 import Todo from "./Todo";
 
 function App() {
@@ -10,16 +10,49 @@ function App() {
     { id: 1, title: "html 배우기", checked: false },
     { id: 2, title: "css 배우기", checked: false },
   ]);
+
+  const inputRef = useRef(null);
+
+  const nextId = useMemo(() => {
+    const maxId = todo.reduce((acc, current) => {
+      return Math.max(acc, current.id);
+    }, 0);
+
+    return maxId + 1;
+  }, [todo]);
+
+  const addTodo = title => {
+    const newTodo = {
+      id: nextId,
+      title,
+      checked: false,
+    };
+
+    setTodo(prev => [...prev, newTodo]);
+  };
   return (
     <>
       <div className="container">
         <h1>My todo App</h1>
-        <Form>
+        <Form
+          onSubmit={e => {
+            e.preventDefault();
+            addTodo(e.target.todo.value);
+            inputRef.current.value = "";
+          }}
+        >
           <Form.Group className="mb-3" controlId="todoInput">
             <Form.Label>할일 입력</Form.Label>
-            <Form.Control type="text" placeholder="할일을 입력하세요." />
+            <Form.Control
+              ref={inputRef}
+              type="text"
+              name="todo"
+              placeholder="할일을 입력하세요."
+            />
           </Form.Group>
-          <Button variant="primary">입력</Button>
+          <Button type="submit" variant="primary">
+            입력
+          </Button>
         </Form>
         <hr />
         {todo.map((item, idx) => {
